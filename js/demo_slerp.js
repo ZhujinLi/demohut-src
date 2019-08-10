00faad550816e90e72df40027c9af667f0e3cc25
ZhuJin Li <lizhujin@outlook.com>
2019-08-11 01:15:18 +0800

    Remove webpack

---
+++ js/demo_slerp.js
@@ -0,0 +1,27 @@
+import './three.js';
+
+let w = 600;
+let h = 400;
+
+let camera = new THREE.PerspectiveCamera(45, w / h, 0.1, 1000);
+camera.position.z = 5;
+
+let scene = new THREE.Scene();
+
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+document.body.appendChild(renderer.domElement);
+
+function animate() {
+	cube.rotation.x += 0.05;
+	cube.rotation.y += 0.05;
+	renderer.render(scene, camera);
+	requestAnimationFrame(animate);
+}
+
+animate();
