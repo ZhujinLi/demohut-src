7c88c3e7684db879c19d2d913606585c5e96b0c3
ZhuJin Li <lizhujin@outlook.com>
2019-08-10 18:01:26 +0800

    Layout

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/demo_slerp.js',
+  entry: [
+    './src/index.js'
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'demo_slerp.html',
+      template: './src/demo_slerp.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  performance: {
+    // Since some js library is too large
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000
+  },
   mode: 'production'
 };
-
