46b7ace2ec1ff03e49ee724948ddc14b92c2d4c9
ZhuJin Li <lizhujin@outlook.com>
2019-08-25 02:26:05 +0800

    Remove gesture

--- public/javascripts/subj-quaternions.js
+++ public/javascripts/subj-quaternions.js
@@ -1,5 +1,4 @@
 import '/three/build/three.min.js';
-import { TrackballControls } from '/three/examples/jsm/controls/TrackballControls.js';
 import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
 
 function showViewSimple() {
@@ -8,11 +7,12 @@ function showViewSimple() {
 
 	let camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000);
 	camera.position.set(5, 5, 5);
+	camera.lookAt(0, 0, 0);
 	camera.up.set(0, 1, 0);
 
 	let scene = new THREE.Scene();
 
-	// ???
+	// ???: The model will fail to appear if commented out
 	new THREE.BoxGeometry(1, 1, 1);
 
 	let loader = new GLTFLoader();
@@ -20,10 +20,6 @@ function showViewSimple() {
 		'/models/Jet/Jet.gltf',
 		function (gltf) {
 			scene.add(gltf.scene);
-		},
-		function () { },
-		function (error) {
-			console.error(error);
 		}
 	);
 
@@ -37,12 +33,7 @@ function showViewSimple() {
 	renderer.setSize(w, h);
 	document.getElementById('view-simple').appendChild(renderer.domElement);
 
-	let controls = new TrackballControls(camera, renderer.domElement);
-	controls.rotateSpeed = 5.0;
-	controls.noZoom = true;
-
 	function animate() {
-		controls.update();
 		renderer.render(scene, camera);
 		requestAnimationFrame(animate);
 	}
