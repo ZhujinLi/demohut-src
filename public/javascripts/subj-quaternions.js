65791c2c0fb3b347823d1ed24a97132064b9432c
ZhuJin Li <lizhujin@outlook.com>
2019-08-25 02:08:37 +0800

    Add gltf model

--- public/javascripts/subj-quaternions.js
+++ public/javascripts/subj-quaternions.js
@@ -1,34 +1,48 @@
 import '/three/build/three.min.js';
-import { OrbitControls } from '/three/examples/jsm/controls/OrbitControls.js';
+import { TrackballControls } from '/three/examples/jsm/controls/TrackballControls.js';
+import { GLTFLoader } from '/three/examples/jsm/loaders/GLTFLoader.js';
 
 function showViewSimple() {
 	let w = 400;
 	let h = 300;
 
 	let camera = new THREE.PerspectiveCamera(60, w / h, 0.1, 1000);
-	camera.position.set(3, 3, 3);
-	camera.up.set(0, 0, 1);
+	camera.position.set(5, 5, 5);
+	camera.up.set(0, 1, 0);
 
 	let scene = new THREE.Scene();
 
-	let geometry = new THREE.BoxGeometry(1, 1, 1);
-	let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-	let cube = new THREE.Mesh(geometry, material);
-	scene.add(cube);
+	// ???
+	new THREE.BoxGeometry(1, 1, 1);
 
-	var axesHelper = new THREE.AxesHelper(1.5);
+	let loader = new GLTFLoader();
+	loader.load(
+		'/models/Jet/Jet.gltf',
+		function (gltf) {
+			scene.add(gltf.scene);
+		},
+		function () { },
+		function (error) {
+			console.error(error);
+		}
+	);
+
+	let ambientLight = new THREE.AmbientLight(0xffffff, 1.0);
+	scene.add(ambientLight);
+
+	let axesHelper = new THREE.AxesHelper(5);
 	scene.add(axesHelper);
 
-	let renderer = new THREE.WebGLRenderer();
+	let renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(w, h);
 	document.getElementById('view-simple').appendChild(renderer.domElement);
 
-	let controls = new OrbitControls(camera, renderer.domElement);
-	controls.target.set(0, 0, 0);
-	controls.enablePan = false;
-	controls.enableZoom = false;
+	let controls = new TrackballControls(camera, renderer.domElement);
+	controls.rotateSpeed = 5.0;
+	controls.noZoom = true;
 
 	function animate() {
+		controls.update();
 		renderer.render(scene, camera);
 		requestAnimationFrame(animate);
 	}
