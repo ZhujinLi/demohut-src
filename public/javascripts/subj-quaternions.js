157fd765df90d9589e1680c9d646f3f58c9dd5af
ZhuJin Li <lizhujin@outlook.com>
2019-09-01 01:48:50 +0800

    Add slerp demo

--- public/javascripts/subj-quaternions.js
+++ public/javascripts/subj-quaternions.js
@@ -1,5 +1,6 @@
 import '/three/build/three.min.js';
 import '/three/examples/js/loaders/GLTFLoader.js';
+import '/three/examples/js/controls/OrbitControls.js';
 
 /* global THREE */
 
@@ -24,7 +25,9 @@ class World {
 			(gltf) => {
 				this._obj = gltf.scene;
 				this._scene.add(gltf.scene);
-				requestAnimationFrame(renderCallback);
+				if (renderCallback) {
+					requestAnimationFrame(renderCallback);
+				}
 			},
 			null,
 			(error) => { console.log(error); }
@@ -34,6 +37,64 @@ class World {
 	scene() { return this._scene; }
 	camera() { return this._camera; }
 	obj() { return this._obj; }
+}
+
+class Sphere {
+	constructor(w, h) {
+		this._scene = new THREE.Scene();
+
+		this._camera = new THREE.PerspectiveCamera(50, w / h, 0.01, 10);
+		this._camera.position.z = 5;
+		this._camera.lookAt(0, 0, 0);
+		this._camera.up.set(0, 1, 0);
+
+		const targetGeometry = new THREE.SphereBufferGeometry(0.1);
+		const targetMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+		this._target = new THREE.Mesh(targetGeometry, targetMaterial);
+		this._scene.add(this._target);
+
+		const sphereMaterialOld = new THREE.MeshBasicMaterial({ color: 0xcccccc, wireframe: true, transparent: true, opacity: 0.3 });
+		this._sphereOld = new THREE.Mesh(new THREE.Geometry(), sphereMaterialOld);
+		this._scene.add(this._sphereOld);
+
+		const sphereMaterialNew = new THREE.MeshBasicMaterial({ color: 0xff8080, wireframe: true, transparent: true, opacity: 0.3 });
+		this._sphereNew = new THREE.Mesh(new THREE.Geometry(), sphereMaterialNew);
+		this._scene.add(this._sphereNew);
+
+		// Since the geometry in a mesh could not be resized, I gave up the attempt of using a dynamic line
+		this._tracePoints = [];
+	}
+
+	scene() { return this._scene; }
+	camera() { return this._camera; }
+
+	setTarget(quat) {
+		const radius = Sphere._radiusFromW(quat.w);
+		this._sphereOld.geometry = this._sphereNew.geometry;
+		this._sphereNew.geometry = new THREE.SphereBufferGeometry(radius, 32, 32);
+
+		const v = Sphere._positionFromQuat(quat);
+		this._target.position.set(v.x, v.y, v.z);
+
+		this._tracePoints.forEach((mesh) => { this._scene.remove(mesh); })
+	}
+
+	addTrace(quat) {
+		const v = Sphere._positionFromQuat(quat);
+		const point = new THREE.Mesh(new THREE.SphereGeometry(0.03, 8, 8), new THREE.MeshBasicMaterial({ color: 0x00ff00 }));
+		point.position.copy(v);
+		this._tracePoints.push(point);
+		this._scene.add(point);
+	}
+
+	static _radiusFromW(w) { return w + 1; }
+
+	static _positionFromQuat(quat) {
+		const v = new THREE.Vector3(quat.x, quat.y, quat.z);
+		v.setLength(Sphere._radiusFromW(quat.w));
+		return v;
+	}
+
 }
 
 function showDemoQuat() {
@@ -151,3 +212,98 @@ function showDemoQuat() {
 }
 
 showDemoQuat();
+
+function showDemoSlerp() {
+	const INTERVAL_MS = 2000;
+	let rendererL;
+	let rendererR;
+	let world;
+	let sphere;
+	let currentQuat;
+	let targetQuat;
+	let clock;
+
+	initView();
+	generateTarget();
+	requestAnimationFrame(render);
+
+	function render() {
+		const elapsedS = clock.getElapsedTime();
+		const elapsedMs = elapsedS * 1000;
+		const progress = elapsedMs / INTERVAL_MS;
+
+		const quat = new THREE.Quaternion();
+		THREE.Quaternion.slerp(currentQuat, targetQuat, quat, progress);
+		quat.normalize();
+
+		const obj = world.obj();
+		obj && obj.setRotationFromQuaternion(quat);
+
+		sphere.addTrace(quat);
+
+		rendererL.render(world.scene(), world.camera());
+		rendererR.render(sphere.scene(), sphere.camera());
+
+		requestAnimationFrame(render);
+	}
+
+	function initView() {
+		initViewLeft();
+		initViewRight();
+	}
+
+	function initViewLeft() {
+		const w = 300;
+		const h = 200;
+
+		rendererL = new THREE.WebGLRenderer({ antialias: true });
+		rendererL.setPixelRatio(window.devicePixelRatio);
+		rendererL.setSize(w, h);
+		document.getElementById('view-slerp-world').appendChild(rendererL.domElement);
+
+		world = new World(w, h);
+
+		clock = new THREE.Clock(false);
+
+		targetQuat = new THREE.Quaternion();
+		targetQuat.set(1, 0, 0, 0);
+	}
+
+	function initViewRight() {
+		const w = 300;
+		const h = 200;
+
+		rendererR = new THREE.WebGLRenderer({ antialias: true });
+		rendererR.setPixelRatio(window.devicePixelRatio);
+		rendererR.setSize(w, h);
+		document.getElementById('view-slerp-sphere').appendChild(rendererR.domElement);
+
+		sphere = new Sphere(w, h);
+
+		new THREE.OrbitControls(sphere.camera(), rendererR.domElement);
+	}
+
+	function generateTarget() {
+		currentQuat = targetQuat;
+
+		targetQuat = new THREE.Quaternion();
+		const axis = new THREE.Vector3(
+			Math.random() - 0.5,
+			Math.random() - 0.5,
+			Math.random() - 0.5
+		);
+		const angle = Math.random() * Math.PI * 2;
+		targetQuat.setFromAxisAngle(axis, angle);
+		targetQuat.normalize();
+
+		sphere.setTarget(targetQuat);
+
+		clock.stop();
+		clock.start();
+
+		setTimeout(generateTarget, INTERVAL_MS);
+	}
+
+}
+
+showDemoSlerp();
