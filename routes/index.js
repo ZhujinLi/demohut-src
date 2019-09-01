856599179aaf045164bfe077a26aac65514f525c
ZhuJin Li <lizhujin@outlook.com>
2019-09-01 23:39:06 +0800

    Fix a typo in headings

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Fum with RTT' });
+  res.render('index', { title: 'fun_with_rtr' });
 });
 
 module.exports = router;
