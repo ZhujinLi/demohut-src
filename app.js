4acca1bf2a2258657d2de6e203a592b945ddf631
ZhuJin Li <lizhujin@outlook.com>
2020-03-29 02:58:58 +0800

    Remove route

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/public', express.static(path.join(__dirname, 'public')), serveIndex(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
