b989f9eb7eb1894f7d8dc43fe29c702dba9f22e6
ZhuJin Li <lizhujin@outlook.com>
2020-08-12 23:28:56 +0800

    Transfer to gitee

--- webpack.config.js
+++ webpack.config.js
@@ -81,7 +81,7 @@ module.exports = {
                 subj_title: subj.title,
                 head_title: subj.title + " - " + MAIN_TITLE,
                 backLink: '/',
-                srcLink: 'https://github.com/ZhujinLi/ZhujinLi.github.io/tree/master/_source_code/src/subjs/' + subj.name,
+                srcLink: 'https://gitee.com/xyuv/xyuv/tree/master/_source_code/src/subjs/' + subj.name,
             }
         }))].concat([
             new HtmlWebpackPlugin({
