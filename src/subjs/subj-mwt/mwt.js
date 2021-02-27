1e06cb2af3cfd3592172a8f97d4556bf08c32755
ZhuJin Li <lizhujin@outlook.com>
2021-02-28 01:53:54 +0800

    All but algorithm

---
+++ src/subjs/subj-mwt/mwt.js
@@ -0,0 +1,13 @@
+
+/**
+ * @param {Array} contour 
+ */
+export function mwt(contour) {
+    return {
+        edges: [
+            [100, 100, 200, 200],
+            [100, 200, 300, 500],
+        ],
+        weight: 123,
+    };
+}
---
+++ src/subjs/subj-mwt/random.js
@@ -0,0 +1,10 @@
+
+export function random(contour) {
+    return {
+        edges: [
+            [200, 100, 200, 200],
+            [400, 200, 300, 500],
+        ],
+        weight: 456,
+    };
+}
