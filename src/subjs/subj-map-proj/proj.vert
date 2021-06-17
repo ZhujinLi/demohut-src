ec518de1e70e6b49946caeea3c3ab7f2eb6925c5
ZhuJin Li <lizhujin@outlook.com>
2021-06-17 21:54:19 +0800

    Add Winkel III projection

--- src/subjs/subj-map-proj/proj.vert
+++ src/subjs/subj-map-proj/proj.vert
@@ -2,24 +2,31 @@
 uniform float u_ratioGlobe;
 uniform float u_ratioEquirectangular;
 uniform float u_ratioMercator;
+uniform float u_ratioWinkelIII;
 
 varying vec2 v_uv;
 
 const float PI = 3.14159265;
 const float DEG2RAD = PI / 180.0;
+const float GLOBE_RADIUS = 2.0 / PI;	// To make the globe's equatorial circumference equal to 4,
+										// which is the width of equirectangle projection.
+
+float sinc(float x) {
+	return sin(x) / x;
+}
 
 vec3 globe(float lon, float lat) {
 	float z = sin(lat * DEG2RAD);
 	float r = cos(lat * DEG2RAD);
 	float x = r * cos((lon - 90.0) * DEG2RAD);
 	float y = r * sin((lon - 90.0) * DEG2RAD);
-	return vec3(x, y, z);
+	return vec3(x * GLOBE_RADIUS, y * GLOBE_RADIUS, z * GLOBE_RADIUS);
 }
 
 vec3 equirectangular(float lon, float lat) {
 	float x = lon / 90.0;
 	float z = lat / 90.0;
-	return vec3(x, -1.0, z);
+	return vec3(x, -GLOBE_RADIUS, z);
 }
 
 vec3 mercator(float lon, float lat) {
@@ -32,7 +39,32 @@ vec3 mercator(float lon, float lat) {
 	x = (x - 0.5) * 4.0;
 	z = (0.5 - z) * 4.0;
 
-	return vec3(x, -1.0, z);
+	return vec3(x, -GLOBE_RADIUS, z);
+}
+
+vec3 _winkeliiiStd(float lon, float lat) {
+	// Adopting wiki's notations...
+	float lambda = lon * DEG2RAD;
+	float phi = lat * DEG2RAD;
+	float phi1 = acos(2.0 / PI);
+	float alpha = acos(cos(phi) * cos(lambda / 2.0));
+
+	float x = 0.5 * (lambda * cos(phi1) + 2.0 * cos(phi) * sin(lambda / 2.0) / sinc(alpha));
+	float z = 0.5 * (phi + sin(phi) / sinc(alpha));
+
+	return vec3(x, -GLOBE_RADIUS, z);
+}
+
+vec3 winkeliii(float lon, float lat) {
+	vec3 p = _winkeliiiStd(lon, lat);
+
+	// Normalize for display
+	float xmax = _winkeliiiStd(180.0, 0.0).x;
+	float zmax = _winkeliiiStd(0.0, 90.0).z;
+	p.x = p.x / xmax * 2.0;
+	p.z /= zmax;
+	
+	return p;
 }
 
 void main() {
@@ -45,6 +77,7 @@ void main() {
 
 	vec3 pos = u_ratioGlobe * globe(lon, lat)
 		+ u_ratioEquirectangular * equirectangular(lon, lat)
-		+ u_ratioMercator * mercator(lon, lat);
+		+ u_ratioMercator * mercator(lon, lat)
+		+ u_ratioWinkelIII * winkeliii(lon, lat);
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
--- src/subjs/subj-map-proj/subj-map-proj.js
+++ src/subjs/subj-map-proj/subj-map-proj.js
@@ -3,32 +3,33 @@ import "three/examples/js/controls/OrbitControls.js";
 import vert from "./proj.vert";
 import frag from "./proj.frag";
 
-const W = 600;
-const H = 600;
+const W = 700;
+const H = 700;
 const ANIMATION_DURATION_SECONDS = 1.0;
 const ANIMATION_INTERVAL_SECONDS = 0.01;
 const ANIMATION_FRAME_COUNT = ANIMATION_DURATION_SECONDS / ANIMATION_INTERVAL_SECONDS;
 
-// The ratios of three projection methods. Their sum should always be 1.
-let curRatios = [1, 0, 0];
-let ratioStep = [0, 0, 0];
+// The ratios of projection methods. Their sum should always be 1.
+let curRatios = [1, 0, 0, 0];
+let ratioStep = [0, 0, 0, 0];
 
 // Ever running animation timer
 setInterval(() => {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < curRatios.length; i++) {
         curRatios[i] = Math.min(Math.max(curRatios[i] + ratioStep[i], 0), 1);
     }
 }, ANIMATION_INTERVAL_SECONDS * 1000);
 
 function setTarget(target) {
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < curRatios.length; i++) {
         ratioStep[i] = (target[i] - curRatios[i]) / ANIMATION_FRAME_COUNT;
     }
 }
 
-document.getElementById("globe").onclick = () => setTarget([1, 0, 0]);
-document.getElementById("equirectangular").onclick = () => setTarget([0, 1, 0]);
-document.getElementById("mercator").onclick = () => setTarget([0, 0, 1]);
+document.getElementById("globe").onclick = () => setTarget([1, 0, 0, 0]);
+document.getElementById("equirectangular").onclick = () => setTarget([0, 1, 0, 0]);
+document.getElementById("mercator").onclick = () => setTarget([0, 0, 1, 0]);
+document.getElementById("winkeliii").onclick = () => setTarget([0, 0, 0, 1]);
 
 //
 
@@ -38,7 +39,7 @@ renderer.setSize(W, H);
 renderer.setClearColor("#fff");
 
 const camera = new THREE.PerspectiveCamera(30, 1);
-camera.position.set(0, -10, 0);
+camera.position.set(0, -8.5, 0);
 camera.up.set(0, 0, 1);
 camera.lookAt(0, 0, 0);
 
@@ -69,6 +70,7 @@ function animate() {
     mtl.uniforms.u_ratioGlobe = { value: curRatios[0] };
     mtl.uniforms.u_ratioEquirectangular = { value: curRatios[1] };
     mtl.uniforms.u_ratioMercator = { value: curRatios[2] };
+    mtl.uniforms.u_ratioWinkelIII = { value: curRatios[3] };
 
     renderer.render(scene, camera);
 }
